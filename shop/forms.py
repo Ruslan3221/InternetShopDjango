a13f26e5dad3e2b417994525a18e607e2fc1a286
Ruslan3221 <ruslan4528053@mail.ru>
2024-05-18 19:04:31 +0500

    update

--- shop/forms.py
+++ shop/forms.py
@@ -9,4 +9,7 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
 
 class ProductImageForm(forms.Form):
-    images = MultiFileField(min_num=1, max_num=10, max_file_size=1024*1024*5)
+    #images = MultiFileField(min_num=1, max_num=10, max_file_size=1024*1024*5)
+    class Meta:
+        model = ProductImage
+        fields = ['product_images']
